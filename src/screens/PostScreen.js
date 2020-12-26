ae9270fcb1995a3ddf66cb7d69c78a7f2156f6e4
halilkrkn <42476890+halilkrkn@users.noreply.github.com>
2020-12-26 14:11:37 +0300

    Api ile Tasarım işlemleri Bitirildi.
    Newsapi üzerinde api mizi çekerek uygulammaızda gösterdik.

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, View, FlatList} from 'react-native'
 import NewsCard from '../components/NewsCards'
+import newsApi from '../api/News'
 
-const PostScreen = () => {
-  return (
-   
-     <NewsCard/>
-     
+const PostScreen = ({navigation}) => {
+
+
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+      getNewsFromAPI()
+    },[])
+
+  function getNewsFromAPI() {
+    newsApi.get('top-headlines?country=tr&category=science&apiKey=bbc189fcd1ab4a78ae8e75256349d7aa')
+    .then(async function (response) {
+      setNews(response.data)
+            
+    })
+    .catch(function (error) {
+      console.log(error)
+      
+    })
+  }
+
+  if (!news) {
+      return null
+  }
   
+
+
+  return (   
+    <View style={styles.container}>
+     <FlatList
+      data = {news.articles}
+      keyExtractor = {(item , index) => 'key' + index}
+      renderItem = {({item}) => {
+
+        return(<NewsCard item = {item}/>)
+
+      }}/>
+
+    </View>
   )
 }
 
